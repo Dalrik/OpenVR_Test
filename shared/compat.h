9b4ca36a12fbfe78efd9bbda4babbe1f7e0f965a
Ian Thompson <ian@yann.io>
2019-12-07 14:23:01 -0800

    Import OpenVR OpenGL example project

---
+++ shared/compat.h
@@ -0,0 +1,33 @@
+#ifndef OPENVR_SAMPLES_SHARED_COMPAT_H_
+#define OPENVR_SAMPLES_SHARED_COMPAT_H_
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+// Handle non standard code.
+#ifndef _WIN32
+
+#include <cstdbool>
+#include <unistd.h>
+
+#define sprintf_s   snprintf
+#define vsprintf_s  sprintf
+#define _stricmp    strcmp
+#define stricmp     strcmp
+#define strnicmp    strncasecmp
+#define strcpy_s(dst, n, src)   int(strncpy(dst, src, n) != nullptr)
+#define fopen_s(fd, path, mode) int((*fd = fopen(path, mode)) != nullptr)
+#define _vsnprintf_s(buffer, size, fmt, ap)  vsnprintf(buffer, size, fmt, ap)
+#define OutputDebugStringA(x) fprintf(stderr, "%s\n", x)
+
+typedef int errno_t;
+
+#else
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#endif  // _WIN32
+
+#endif  // OPENVR_SAMPLES_SHARED_COMPAT_H_
